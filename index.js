066ca0336817a62d3acde9c056091e06ca158de3
Daniel Jost <daniel.jost@permanent.de>
2015-02-09 18:26:46 +0100

    initial release

---
+++ index.js
@@ -0,0 +1,53 @@
+var fs = require('fs');
+var path = require('path');
+var trim = require('trim');
+var chalk = require('chalk');
+var nodemiral = require('nodemiral');
+
+var SSH_PRIVATE_KEY = fs.readFileSync(process.env.HOME + '/.ssh/id_rsa', 'utf8');
+
+var files = fs.readdirSync(path.resolve(__dirname, 'conf/'));
+
+var config;
+var user;
+var username;
+var sudo;
+var keyPath;
+var privateKey;
+
+for (var i = 0; i < files.length; i++) {
+
+    if (path.extname(files[i]) != '.json') {
+        continue;
+    }
+
+    config = require(path.resolve(__dirname, 'conf/') + '/' + files[i]);
+
+    for (var j = 0; j < config.users.length; j++) {
+
+        user = config.users[j];
+        username = user.name;
+        sudo = user.sudo;
+
+        keyPath = path.resolve(__dirname, 'keydir/') + '/' + username + '.key';
+
+        if (!fs.existsSync(keyPath)) {
+            console.log('[' + config.name + '] key for ' + username + ' not found, skipping');
+            continue;
+        }
+
+        privateKey = fs.readFileSync(keyPath);
+
+        (function(config, username, sudo, privateKey) {
+
+            var session = nodemiral.session(config.address, {username: 'root', pem: SSH_PRIVATE_KEY});
+
+            session.executeScript(path.resolve(__dirname, 'scripts/') + '/handle_user.sh', {username: username, sudo: sudo, private_key: privateKey}, function(err, code, logs) {
+                console.log('[' + config.name + '] user: ' + username + ', sudo: ' + sudo + ', output: ' + chalk.green(trim(logs.stdout)));
+            });
+
+        })(config, username, sudo, privateKey);
+
+    }
+
+}
---
+++ scripts/handle_user.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if ! id -u "<%= username %>" > /dev/null 2>&1; then
+	useradd -m -s /bin/bash "<%= username %>"
+fi
+
+<% if (sudo) { %>
+usermod -a -G sudo "<%= username %>"
+<% } %>
+
+mkdir -p /home/<%= username %>/.ssh/
+echo "<%= private_key %>" > /home/<%= username %>/.ssh/authorized_keys
+
+echo "processing finished"
